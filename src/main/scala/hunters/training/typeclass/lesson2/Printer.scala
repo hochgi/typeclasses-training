5710556ee82c0046b2e0b06b6bcd3d643c5242b8
hochgi <gilad.hoch@hunters.ai>
2023-11-15 16:51:44 +0200

    [lesson2.1] challenge

--- src/main/scala/hunters/training/typeclass/lesson2/Printer.scala
+++ src/main/scala/hunters/training/typeclass/lesson2/Printer.scala
@@ -8,4 +8,8 @@ object Printer {
   implicit val stringIdentityPrinter: Printer[String] = identity[String]
 
   implicit val intPrinter: Printer[Int] = String.valueOf
+
+  implicit def list[A : Printer]: Printer[List[A]] = ???
+
+  implicit def either[L : Printer, R : Printer]: Printer[Either[L, R]] = ???
 }
--- src/test/scala/hunters/training/typeclass/lesson2/TreePrinterTests.scala
+++ src/test/scala/hunters/training/typeclass/lesson2/TreePrinterTests.scala
@@ -15,6 +15,69 @@ class TreePrinterTests extends AnyFunSpec with Matchers {
         val s = TreePrinter.print(Tree("42", Nil))
         s shouldEqual "42"
       }
+
+      it("with yield correct representation for an Either[Int, String] simple tree") {
+        val s = TreePrinter.print[Either[Int, String]](Tree(Left(42), List(Tree(Right("24"), Nil))))
+        s shouldEqual
+          """42
+            |└── 24""".stripMargin
+      }
+    }
+    describe("should also be able to render multi-line strings") {
+      it("with yield correct representation for a single node tree") {
+        val s = TreePrinter.print(Tree(List(4, 2), Nil))
+        s shouldEqual "4\n2"
+      }
+
+      it("with yield correct representation for a simple tree") {
+        val s = TreePrinter.print[List[Either[String, Int]]](Tree(List(Left("4"), Right(2)), List(Tree(List(Right(2),Left("4")), Nil))))
+        s shouldEqual
+          """4
+            |2
+            |└── 2
+            |    4""".stripMargin
+      }
+
+      it("with yield correct representation for nested non-simple tree") {
+        val s = TreePrinter.print[List[String]](
+          Tree(
+            List("1\n1\n1"),
+            List(
+              Tree(
+                List("2", "2\n2"),
+                List(
+                  Tree(
+                    List("3", "3", "3"),
+                    Nil),
+                  Tree(
+                    List("4\n4", "4"),
+                    Nil)
+                )
+              ),
+              Tree(
+                List("5\n5\n5"),
+                Nil)
+            )
+          )
+        )
+
+        s shouldEqual
+          """1
+            |1
+            |1
+            |├── 2
+            |│   2
+            |│   2
+            |│   ├── 3
+            |│   │   3
+            |│   │   3
+            |│   └── 4
+            |│       4
+            |│       4
+            |└── 5
+            |    5
+            |    5""".stripMargin
+      }
     }
   }
 }
