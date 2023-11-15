071cc430422dab2425a051cad3b65a5cd71f50fe
hochgi <gilad.hoch@hunters.ai>
2023-11-15 14:11:07 +0200

    [lesson1.1] solution

--- src/main/scala/hunters/training/typeclass/lesson1/StringTreePrinter.scala
+++ src/main/scala/hunters/training/typeclass/lesson1/StringTreePrinter.scala
@@ -10,5 +10,46 @@ object StringTreePrinter {
    * @param tree the tree to print
    * @return printed string
    */
-  def print(tree: StringTree): String = ???
+  def print(tree: StringTree): String = {
+
+    /**
+     * DFS the tree, and append to the provided StringBuilder
+     *
+     * @param prefix           prepend every line with prefix
+     * @param headPrefixSuffix the current node is prepended with this as well
+     * @param tailPrefixSuffix any child of the current node, has this appended to the prefix
+     * @param sb               the StringBuilder to append to
+     * @param subtree          the current tree node
+     * @return same StringBuilder for convenience chaining
+     */
+    def appendChildRec(prefix: String,
+                       headPrefixSuffix: String,
+                       tailPrefixSuffix: String,
+                       sb: StringBuilder,
+                       subtree: StringTree): StringBuilder = {
+
+      var prefixSuffix = headPrefixSuffix
+      subtree.value.linesIterator.foreach { valueLine =>
+        sb.append(prefix)
+          .append(prefixSuffix)
+          .append(valueLine)
+          .append('\n')
+        prefixSuffix = tailPrefixSuffix
+      }
+
+      subtree match {
+        case StringTree(_, Nil) => sb
+        case StringTree(_, nonEmptyChildren) =>
+          nonEmptyChildren.init.foreach { notTheLast =>
+            appendChildRec(prefix + tailPrefixSuffix, "├── ", "│   ", sb, notTheLast)
+          }
+          appendChildRec(prefix + tailPrefixSuffix, "└── ", "    ", sb, nonEmptyChildren.last)
+          sb
+      }
+    }
+
+    appendChildRec("", "", "", new StringBuilder(), tree)
+      .dropRight(1) // every line is appended with '\n', so finally drop the last one.
+      .result()
+  }
 }
