0634d371aeb107fe3f9949feb961b3ed20af7d68
hochgi <gilad.hoch@hunters.ai>
2023-11-16 09:47:13 +0200

    [lesson3.1] challenge

--- src/main/scala/hunters/training/typeclass/lesson3/TreePrinter.scala
+++ src/main/scala/hunters/training/typeclass/lesson3/TreePrinter.scala
@@ -1,39 +1,43 @@
 package hunters.training.typeclass.lesson3
 
+
+/**
+ * To make [[TreePrinter]]s even more composable and simple,
+ * instead of forcing library users to know (and use) [[TreePrinter.Tree]],
+ * we can add composability to our typeclass, and only keeping the tree internally.
+ * This way, we can compose complex structures tree representations even more easily.
+ */
 trait TreePrinter[T] {
 
-  /**
-   * To make [[TreePrinter]]s more composable,
-   * instead of [[String]], we return a [[TreePrinter.Tree]] whose value is always just a list of lines.
-   * This way, we can compose complex structures tree representations more easily.
-   *
-   * @param t Something that can be made to a tree
-   * @return Tree
-   */
-  def print(t: T): TreePrinter.Tree
+  def print(t: T): String = ???
+
+  def contramap[U](f: U => T): TreePrinter[U] = ???
+
+  protected def mkTree(t: T): TreePrinter.Tree
+
+  private def toLines(subtree: TreePrinter.Tree): List[String] = subtree match {
+    case TreePrinter.Tree(lines, Nil) => lines
+    case TreePrinter.Tree(lines, nonEmptyChildren) =>
+      val last = toLines(nonEmptyChildren.last)
+      val prefixedLast = ("└── " + last.head) :: last.tail.map("    ".+)
+      val prefixedInit = nonEmptyChildren.init.map { t =>
+        val notTheLast = toLines(t)
+        ("├── " + notTheLast.head) :: notTheLast.tail.map("│   ".+)
+      }
+      lines ::: prefixedInit.foldRight(prefixedLast)(_ ::: _)
+  }
 }
+
 object TreePrinter {
 
   case class Tree(valueLines: List[String], children: List[Tree])
 
-  def printTree[T : TreePrinter](t: T): String = {
+  def printTree[T : TreePrinter](t: T): String = ???
 
-    def toLines(subtree: Tree): List[String] = subtree match {
-      case Tree(lines, Nil) => lines
-      case Tree(lines, nonEmptyChildren) =>
-        val last = toLines(nonEmptyChildren.last)
-        val prefixedLast = ("└── " + last.head) :: last.tail.map("    ".+)
-        val prefixedInit = nonEmptyChildren.init.map { t =>
-          val notTheLast = toLines(t)
-          ("├── " + notTheLast.head) :: notTheLast.tail.map("│   ".+)
-        }
+  // TODO: #1 - write TreePrinters for all primitives
+  // TODO: #2 - Augment with typeclass constructors for stdlib constructs (collections, Option, Try, Either, Tuples, ...)
 
-        lines ::: prefixedInit.foldRight(prefixedLast)(_ ::: _)
-    }
-
-    toLines(implicitly[TreePrinter[T]].print(t)).mkString("\n")
-  }
-
+  implicit val treeTreePrinter: TreePrinter[Tree] = ???
 
   implicit val intsTreePrinter: TreePrinter[Int] = (i: Int) => Tree(String.valueOf(i) :: Nil, Nil)
 
@@ -41,9 +45,15 @@ object TreePrinter {
 
   implicit def tuple2TreePrinter[A : TreePrinter, B : TreePrinter]: TreePrinter[(A, B)] = {
     val tupleValueLines: List[String] = List(classOf[(_, _)].getSimpleName)
+    val aTreePrinter: TreePrinter[A] = implicitly[TreePrinter[A]]
+    val bTreePrinter: TreePrinter[B] = implicitly[TreePrinter[B]]
 
-    { case (a, b) =>
-      Tree(tupleValueLines, List(implicitly[TreePrinter[A]].print(a), implicitly[TreePrinter[B]].print(b)))
+    new TreePrinter[(A, B)] {
+      override protected def mkTree(t: (A, B)): Tree = Tree(
+        tupleValueLines,
+        List(
+          aTreePrinter.mkTree(t._1),
+          bTreePrinter.mkTree(t._2)))
     }
   }
 }
